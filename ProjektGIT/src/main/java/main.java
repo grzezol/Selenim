0fc226607dc4884fcfc5ea89519b672409a2b810
Przemyslaw Grzezolka (Nokia) <przemyslaw.grzezolka@nokia.com>
2023-12-07 16:28:43 +0100

    Drugi user zmeinia z Eloooo Mariusz

--- ProjektGIT/src/main/java/main.java
+++ ProjektGIT/src/main/java/main.java
@@ -1,6 +1,6 @@
 public class main {
     public static void main(String[] args) {
-        System.out.println("elooo Polska");
+        System.out.println("elooo Mariusz");
     }
 	public static void main(String[] args) {
         System.out.println("witaaaaam dodaje drugi user");
