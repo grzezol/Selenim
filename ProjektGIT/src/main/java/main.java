babb5b98ff3c0a5b2b0d267c9a6f51e347e22a2b
Przemyslaw Grzezolka (Nokia) <przemyslaw.grzezolka@nokia.com>
2023-12-07 16:14:18 +0100

    drugi user dodaje wiiitam funkcje

--- ProjektGIT/src/main/java/main.java
+++ ProjektGIT/src/main/java/main.java
@@ -2,4 +2,7 @@ public class main {
     public static void main(String[] args) {
         System.out.println("elooo");
     }
+	public static void main(String[] args) {
+        System.out.println("witaaaaam dodaje drugi user");
+    }
 }
